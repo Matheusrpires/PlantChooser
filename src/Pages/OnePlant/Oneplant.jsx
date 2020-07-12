c2a440714c39ada80360f600fbb7f1c727268609
Matheus Rodrigues <matheusr@DESKTOP-SO7D7VB.localdomain>
2020-07-11 23:00:52 -0300

    Making responsive

--- src/Pages/OnePlant/Oneplant.jsx
+++ src/Pages/OnePlant/Oneplant.jsx
@@ -13,63 +13,75 @@ const Oneplant = () => {
   const [send, setSend] = useState(false);
 
   const sendEmail = (e) => {
-    e.preventDefault()
-    setSend(true)
-  }
+    e.preventDefault();
+    setSend(true);
+  };
 
   return (
     <div className={styles.container}>
-      <Header color="true" />
-      <div className={styles.container__data}>
-        <div className={styles.picked__container}>
-          <p className={styles.picked__title}>Succulent bowl</p>
-          <p className={styles.picked__price}>$20</p>
-        </div>
-        <img src={plantTeste} className={styles.imgteste} alt="picked plant" />
-        <div className={styles.picked__img}></div>
-        <div className={styles.itens_container}>
-          <img src={testeIcon} atl="effect" />
-          <a>High sunlight</a>
-        </div>
-        <div className={styles.itens_container}>
-          <img src={testeIcon} atl="effect" />
-          <a>High sunlight</a>
-        </div>
-        <div className={styles.itens_container}>
-          <img src={testeIcon} atl="effect" />
-          <a>High sunlight</a>
-        </div>
+      <div className={styles.temporary}>
+        <Header color="true" />
       </div>
-      <div className={styles.email__container}>
-        {send ? (
-          <div className={styles.send__container}>
-            <p className={styles.picked__title}>Thank you!</p>
-            <p className={styles.email__sub}>
-              You will hear from us soon. Please check your e-mail!
-            </p>
-            <img src={envelope} alt='Envelope com planta dentro' />
+      <div className={styles.container__conteudo}>
+        <div className={styles.container__data}>
+          <div className={styles.picked__container}>
+            <p className={styles.picked__title}>Succulent bowl</p>
+            <p className={styles.picked__price}>$20</p>
           </div>
-        ) : (
-          <>
-            <div className={styles.picked__container}>
-              <p className={styles.picked__title}>Nice pick!</p>
+          <img
+            src={plantTeste}
+            className={styles.imgteste}
+            alt="picked plant"
+          />
+          <div className={styles.picked__img}></div>
+          <div className={styles.itens_container}>
+            <img src={testeIcon} atl="effect" />
+            <a>High sunlight</a>
+          </div>
+          <div className={styles.itens_container}>
+            <img src={testeIcon} atl="effect" />
+            <a>High sunlight</a>
+          </div>
+          <div className={styles.itens_container}>
+            <img src={testeIcon} atl="effect" />
+            <a>High sunlight</a>
+          </div>
+        </div>
+        <div className={styles.email__container}>
+          {send ? (
+            <div className={styles.send__container}>
+              <p className={styles.picked__title}>Thank you!</p>
               <p className={styles.email__sub}>
-                Tell us your name and <br />
-                e-mail and we will get in touch about your order ; &#x00029;
+                You will hear from us soon. Please check your e-mail!
               </p>
+              <img src={envelope} alt="Envelope com planta dentro" />
             </div>
-            <form>
-              <label>Name</label>
-              <input placeholder="Crazy Plant Person" />
-              <label>E-mail</label>
-              <input placeholder="plantperson@email.com" />
+          ) : (
+            <>
+              <div className={styles.picked__container}>
+                <p className={styles.picked__title}>Nice pick!</p>
+                <p className={styles.email__sub}>
+                  Tell us your name and <br />
+                  e-mail and we will get in touch about your order ; &#x00029;
+                </p>
+              </div>
+              <form>
+                <label>Name</label>
+                <input placeholder="Crazy Plant Person" />
+                <label>E-mail</label>
+                <input placeholder="plantperson@email.com" />
 
-              <button type="submit" onClick={sendEmail} className={styles.container__button}>
-                send
-              </button>
-            </form>
-          </>
-        )}
+                <button
+                  type="submit"
+                  onClick={sendEmail}
+                  className={styles.container__button}
+                >
+                  send
+                </button>
+              </form>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/Plant/Card.jsx
+++ src/components/Plant/Card.jsx
@@ -12,6 +12,7 @@ import testeIcon3 from '../../assets/icons/grey/toxic.svg';
 const Card = () => {
   return (
     <ul className={styles.categoriesContainer}>
+      
       <li className={styles.categoriesContainer_categorie}>
         <div className={styles.card__container}>
             <img src={plantTeste} />
@@ -53,6 +54,27 @@ const Card = () => {
       </li>
 
       <li className={styles.categoriesContainer_categorie}>
+        <div className={styles.card__container}>
+          <div>
+            <img src={plantTeste} />
+          </div>
+          <div className={styles.card__details}>
+            <p>Lucky bamboo</p>
+            <div className={styles.card__price}>
+              <p>$30</p>
+              <img src={testeIcon} />
+              <img src={testeIcon3} />
+            </div>
+            <Link to='/' className={styles.container__button}>
+              <div className={styles.container}>
+                buy now
+          </div>
+            </Link>
+          </div>
+        </div>
+      </li>
+
+       <li className={styles.categoriesContainer_categorie}>
         <div className={styles.card__container}>
           <div>
             <img src={plantTeste} />
@@ -73,6 +95,20 @@ const Card = () => {
         </div>
       </li>
     </ul>
+//     <div className="cards">
+//   <div className="card">ONE</div>
+//   <div className="card">TWO</div>
+//   <div className="card">THREE</div>
+//   <div className="card">FOUR</div>
+//   <div className="card">FIVE</div>
+//   <div className="card">SIX</div>
+//   <div className="card">SEVEN</div>
+//   <div className="card">EIGHT</div>
+//   <div className="card">NINE</div>
+//   <div className="card">TEN</div>
+//   <div className="card">ELEVEN</div>
+//   <div className="card">TWELVE</div>
+// </div>
   );
 };
 
